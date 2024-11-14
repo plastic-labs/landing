be49a7de4305dd6186afdd164396bbf434824008
David McKelvey <david@mckelveycreative.co>
2024-11-14 14:09:48 -0800

    add themes to storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,14 @@
 import type { Preview } from '@storybook/react'
 import { withThemeFromJSXProvider } from '@storybook/addon-themes'
+import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from '../styles/global-styles'
+import { themes } from '../styles/themes'
 
 export const decorators = [
   withThemeFromJSXProvider({
+    themes,
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
     GlobalStyles,
   }),
 ]
