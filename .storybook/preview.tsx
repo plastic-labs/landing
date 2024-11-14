710854f2402fdb59cb146382422662cb4caaa1a1
David McKelvey <david@mckelveycreative.co>
2024-11-14 14:33:40 -0800

    add viewports

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,6 +4,44 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyles } from '../styles/global-styles'
 import { themes } from '../styles/themes'
 
+const viewports = {
+  mobileSe: {
+    name: 'Mobile SE',
+    styles: {
+      width: '375px',
+      height: '844px',
+    },
+  },
+  mobileXr: {
+    name: 'Mobile',
+    styles: {
+      width: '414px',
+      height: '896px',
+    },
+  },
+  tablet: {
+    name: 'Tablet',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  laptop: {
+    name: 'Laptop',
+    styles: {
+      width: '1280px',
+      height: '982px',
+    },
+  },
+  laptopWide: {
+    name: 'Laptop (Wide)',
+    styles: {
+      width: '1440px',
+      height: '1024px',
+    },
+  },
+}
+
 export const decorators = [
   withThemeFromJSXProvider({
     themes,
@@ -22,6 +60,9 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports,
+    }
   },
 }
 
