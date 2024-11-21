5427c65bc9b31ae7166a774558cb4ded47909586
David McKelvey <david@mckelveycreative.co>
2024-11-21 13:29:32 -0800

    products section base

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 
 import { GetInTouchSection } from '@/components/sections/get-in-touch'
 import { PartnersSection } from '@/components/sections/partners'
+import { ProductsSection } from '@/components/sections/products'
 import { TeamSection } from '@/components/sections/team'
 
 const Page = () => {
   return (
     <>
+      <ProductsSection />
       <TeamSection />
       <PartnersSection />
       <GetInTouchSection />
---
+++ components/sections/products/products.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import styled from 'styled-components'
+import { Title } from '@/components/core/title'
+import { LinkProduct } from '@/components/interactive/links/link-product'
+import { Section } from '@/components/layout/section'
+import { THIN_BREAKPOINT } from '@/styles/breakpoints'
+
+const StyledProductsSection = styled(Section)`
+  position: relative;
+  display: grid;
+  grid-template-columns: 1fr;
+
+  @media (min-width: ${THIN_BREAKPOINT}rem) {
+    padding: 1rem;
+  }
+`
+
+const StyledProductsInner = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1.875rem;
+  padding: 1.875rem 1rem;
+  color: var(--color-primary-surface);
+  background: var(--color-primary-surface-contrast);
+
+  @media (min-width: ${THIN_BREAKPOINT}rem) {
+    grid-gap: 0;
+    padding: 2.5rem 3.375rem;
+    min-height: 37.5rem;
+    border-radius: 0.25rem;
+  }
+`
+
+export const ProductsSection: React.FC = () => {
+  return (
+    <StyledProductsSection>
+      <StyledProductsInner>
+        <Title title="Our Products" subItems={['Honcho', 'Bloom', 'Yousim']} />
+      </StyledProductsInner>
+    </StyledProductsSection>
+  )
+}
